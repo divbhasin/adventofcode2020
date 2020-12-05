a15efad9b2c24945699d2312c981b798d9ce3582
div_bhasin <divbest99@gmail.com>
2020-12-05 14:46:52 -0500

    finished day 3 part 1

--- day3/tree_count.go
+++ day3/tree_count.go
@@ -1,9 +1,51 @@
 package main
 
-func parseMap(fileName string) [][]string {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
 
+func parseMap(fileName string) [][]string {
+	file, err := os.Open(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var board [][]string
+	for scanner.Scan() {
+		row_str := scanner.Text()
+		row := strings.Split(row_str, "")
+		board = append(board, row)
+	}
+
+	return board
+}
+
+func computeTrees(terrain [][]string) int {
+	m, n := len(terrain), len(terrain[0])
+	j := 0
+	trees := 0
+
+	for i := 0; i < m; i++ {
+		item := terrain[i][j%n]
+		if item == "#" {
+			trees += 1
+		}
+
+		j += 3
+	}
+
+	return trees
 }
 
 func main() {
-	terrain = parseMap("input.txt")
+	terrain := parseMap("input.txt")
+	trees := computeTrees(terrain)
+	fmt.Println(trees)
 }
