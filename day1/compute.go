ffd9193619249ed2ecae83c93539826ed70a853c
div_bhasin <divbest99@gmail.com>
2020-12-04 22:43:48 -0500

    solved part 1

--- day1/compute.go
+++ day1/compute.go
@@ -1,19 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
 
-func readFile(name string) {
+func readFile(name string) []int {
+	file, err := os.Open(name)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
+	scanner := bufio.NewScanner(file)
+	var nums []int
+	for scanner.Scan() {
+		num, err := strconv.Atoi(scanner.Text())
+		if err != nil {
+			log.Fatal(err)
+		}
+		nums = append(nums, num)
+	}
+
+	return nums
 }
 
 func main() {
-	nums = readFile("input.txt")
-	m = make(map[int]bool)
+	nums := readFile("input.txt")
+	m := make(map[int]bool)
 
-	for i, elem := range nums {
-		if val, ok := m[2020-elem]; ok {
+	for _, elem := range nums {
+		if _, ok := m[2020-elem]; ok {
 			fmt.Println((2020 - elem) * elem)
 		}
+
+		m[elem] = true
 	}
-	fmt.Println("vim-go")
 }
