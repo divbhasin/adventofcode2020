f674a16e81c3a9b0a8ca14b84b8567ae7668a427
div_bhasin <divbest99@gmail.com>
2020-12-21 21:50:34 -0500

    tryna do day 7 still

--- day7/handy_haversacks.go
+++ day7/handy_haversacks.go
@@ -19,16 +19,18 @@ func parseBagMappings(fileName string) map[string][]string {
 
 	for scanner.Scan() {
 		line := scanner.Text()
-		bag_info := strings.Split(line, "contain")
-		bag := bag_info[0]
-		neighbour_info := strings.Split(bag_info[1], ",")
-		for _, info := range neighbour_info {
-			adj_bag := info[3 : strings.LastIndex(info, "bag")-1]
-			fmt.Println(adj_bag)
-			if _, ok := adj[bag]; !ok {
-				adj[bag] = []string{adj_bag}
-			} else {
-				adj[bag] = append(adj[bag], adj_bag)
+		bag_info := strings.Split(line, " contain ")
+		bag := bag_info[0][0 : strings.LastIndex(bag_info[0], "bags")-1]
+		if bag_info[1] != "no other bags." {
+			neighbour_info := strings.Split(bag_info[1], ",")
+
+			for _, info := range neighbour_info {
+				adj_bag := strings.TrimSpace(info[2 : strings.LastIndex(info, "bag")-1])
+				if _, ok := adj[bag]; !ok {
+					adj[bag] = []string{adj_bag}
+				} else {
+					adj[bag] = append(adj[bag], adj_bag)
+				}
 			}
 		}
 	}
@@ -36,10 +38,16 @@ func parseBagMappings(fileName string) map[string][]string {
 	return adj
 }
 
-func visited(visited, adj, col) {
-	visited[col] = true
-	for _, adj_col := range adj[col] {
+func visit(visited map[string]bool, adj map[string][]string, col string, count *int) {
+	if col == "shiny gold" {
+		*count += 1
+		return
+	}
 
+	for _, adj_col := range adj[col] {
+		if !visited[adj_col] {
+			visit(visited, adj, adj_col, count)
+		}
 	}
 }
 
@@ -47,19 +55,15 @@ func main() {
 	adj := parseBagMappings("input.txt")
 
 	// We have adj, now we can do DFS
-	visited := []bool{}
+	visited := make(map[string]bool)
 	for key, _ := range adj {
 		visited[key] = false
 	}
 
-	visit(visited, adj, "shiny gold")
 	reachable := 0
-
-	for _, val := range visited {
-		if val == true {
-			reachable += 1
-		}
+	for k, _ := range adj {
+		visited[k] = true
+		visit(visited, adj, k, &reachable)
 	}
-
 	fmt.Println(reachable)
 }
