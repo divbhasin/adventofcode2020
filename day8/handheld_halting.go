1a44214b9d7c1cc14a79faa92605a868263e5f09
div_bhasin <divbest99@gmail.com>
2020-12-22 16:54:38 -0500

    done day 8 part 1

--- day8/handheld_halting.go
+++ day8/handheld_halting.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func getInstructions(fileName string) []string {
@@ -15,23 +17,56 @@ func getInstructions(fileName string) []string {
 
 	var ins []string
 
-	scanner = bufio.NewScanner(file)
+	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line = scanner.Text()
+		line := scanner.Text()
 		ins = append(ins, line)
 	}
 
 	return ins
 }
 
-func execInstructions(ins []string, acc *int) {
+func find(i int, arr []int) bool {
+	for _, val := range arr {
+		if val == i {
+			return true
+		}
+	}
 
+	return false
+}
+
+func execInstructions(ins []string, acc *int) {
+	pc := 0
+	var execd []int
+
+	for pc < len(ins) {
+		curr_ins := ins[pc]
+		num, err := strconv.Atoi(curr_ins[4:])
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		execd = append(execd, pc)
+
+		if strings.HasPrefix(curr_ins, "acc") {
+			*acc += num
+		} else if strings.HasPrefix(curr_ins, "jmp") {
+			if find(pc+num, execd) {
+				return
+			}
+			pc += (num - 1)
+		}
+
+		pc += 1
+	}
 }
 
 func main() {
 	ins := getInstructions("input.txt")
 	acc := 0
-	execInstructions(ins, acc)
+	execInstructions(ins, &acc)
 
 	fmt.Println(acc)
 }
