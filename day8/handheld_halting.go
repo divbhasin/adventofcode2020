a3328bf756ce25d2d1567d824d3a35fa71f0b299
div_bhasin <divbest99@gmail.com>
2020-12-22 19:05:29 -0500

    finished day 8 part 2

--- day8/handheld_halting.go
+++ day8/handheld_halting.go
@@ -36,7 +36,7 @@ func find(i int, arr []int) bool {
 	return false
 }
 
-func execInstructions(ins []string, acc *int) {
+func execInstructions(ins []string, acc *int) int {
 	pc := 0
 	var execd []int
 
@@ -54,13 +54,15 @@ func execInstructions(ins []string, acc *int) {
 			*acc += num
 		} else if strings.HasPrefix(curr_ins, "jmp") {
 			if find(pc+num, execd) {
-				return
+				return pc + num
 			}
 			pc += (num - 1)
 		}
 
 		pc += 1
 	}
+
+	return pc
 }
 
 func main() {
@@ -68,5 +70,32 @@ func main() {
 	acc := 0
 	execInstructions(ins, &acc)
 
-	fmt.Println(acc)
+	fmt.Printf("Part 1: %d\n", acc)
+
+	// Part 2
+	// We can brute-force to check which instruction is preventing halting. In
+	// particular, we can iterate through the instructions, change exactly one
+	// nop to a jmp or jmp to a nop, and then run execInstructions. If the pc
+	// can go past the last instruction, we have found the instruction we needed
+	// to halt the program.
+
+	for i, _ := range ins {
+		curr_acc := 0
+		prev := ins[i]
+
+		if strings.HasPrefix(ins[i], "jmp") {
+			ins[i] = "nop" + ins[i][3:]
+		} else if strings.HasPrefix(ins[i], "nop") {
+			ins[i] = "jmp" + ins[i][3:]
+		}
+
+		if !strings.HasPrefix(ins[i], "acc") {
+			pc := execInstructions(ins, &curr_acc)
+			if pc == len(ins) {
+				fmt.Printf("Part 2: %d\n", curr_acc)
+			}
+		}
+
+		ins[i] = prev
+	}
 }
